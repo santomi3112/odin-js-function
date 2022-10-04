c246a1ad00b3467b0a76c3d28bd6be4d8db9044d
santomi3112 <adiprasetio007@gmail.com>
2022-10-04 10:31:33 +0700

    all done

---
+++ script.js
@@ -0,0 +1,19 @@
+function add7 (n) {
+    return n + 7
+}
+
+function multiply (n1,n2) {
+    return n1 * n2
+}
+
+
+//first letter of a string uppercase, but not change the case of any of the other letters
+function capitalize (apaaja) {
+    return apaaja.charAt(0).toUpperCase() + apaaja.slice(1)
+}
+
+
+//takes a string and returns the very last letter of that string: 
+function lastLetter (last) {
+     return last.slice(-1)
+}
